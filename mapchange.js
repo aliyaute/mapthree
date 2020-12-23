3e1e0987fe769c1fd25b9b5ba736abf0e1ed6f93
Aliya <au2237@columbia.edu>
2020-12-23 14:11:54 -0500

    Fourth commit

--- mapchange.js
+++ mapchange.js
@@ -18,7 +18,7 @@ map.on('load', function() {
         'type': 'fill',
         'source': {
             'type': 'geojson',
-            'data': 'data/ChangeOverTimeOne.geojson'
+            'data': 'data/map_two_Data.geojson'
         },
         'layout': {
             // make layer visible by default
@@ -39,31 +39,31 @@ map.on('load', function() {
                 "fill-outline-color": "#ffffff"
         }
     }, 'landuse'); // Here's where we tell Mapbox where to slot this new layer
-    map.addLayer({
-        'id': 'August',
-        'type': 'fill',
-        'source': {
-            'type': 'geojson',
-            'data': 'data/NM_Data..geojson'
-        },
-        'layout': {
-            // make layer visible by default
-            'visibility': 'visible',
-            },
-            'paint': {
-                'fill-color': [
-                    'case', 
-                    ['==',['get', 'Percent_Category'], 1], '#a50f15',
-                    ['==',['get', 'Percent_Category'], 2], '#fdae6b',
-                    ['==',['get', 'Percent_Category'], 3], '#fcbba1',
-                    ['==',['get', 'Percent_Category'], 4], '#cb181d', 
-                    ['==',['get', 'Percent_Category'], 5], '#fdd0a2', 
+    // map.addLayer({
+    //     'id': 'August',
+    //     'type': 'fill',
+    //     'source': {
+    //         'type': 'geojson',
+    //         'data': 'data/NM_Data..geojson'
+    //     },
+    //     'layout': {
+    //         // make layer visible by default
+    //         'visibility': 'visible',
+    //         },
+    //         'paint': {
+    //             'fill-color': [
+    //                 'case', 
+    //                 ['==',['get', 'Percent_Category'], 1], '#a50f15',
+    //                 ['==',['get', 'Percent_Category'], 2], '#fdae6b',
+    //                 ['==',['get', 'Percent_Category'], 3], '#fcbba1',
+    //                 ['==',['get', 'Percent_Category'], 4], '#cb181d', 
+    //                 ['==',['get', 'Percent_Category'], 5], '#fdd0a2', 
 
-                    '#d3d3d3',
-                ],
-                "fill-outline-color": "#ffffff"
-        }
-    }, 'landuse'); // Here's where we tell Mapbox where to slot this new layer
+    //                 '#d3d3d3',
+    //             ],
+    //             "fill-outline-color": "#ffffff"
+    //     }
+    // }, 'landuse'); // Here's where we tell Mapbox where to slot this new layer
 
 });
 
